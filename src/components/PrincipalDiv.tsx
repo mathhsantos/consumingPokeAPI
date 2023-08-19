981eca06732c1b8426ddb874d4daf8402829214c
Matheus Henrique <matt.hcsantos@gmail.com>
2023-08-18 22:37:03 -0300

    feat: first layout pokedex

---
+++ src/components/PrincipalDiv.tsx
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+export const PrincipalDiv = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  position: relative;
+  background: white url('/bg.png') bottom;
+`
